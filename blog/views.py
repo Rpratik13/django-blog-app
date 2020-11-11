db49c3205a8f79c192437b8c18509f027618d0c0
Pratik Rajbhandari <r.pratik013@gmail.com>
2020-11-11 19:32:42 +0545

    Add templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+  {
+    'author' : 'Pratik',
+    'title' : 'Blog Post 1',
+    'content' : 'This is the content.',
+    'date_posted' : 'November 11, 2020'
+  },
+  {
+    'author' : 'Pratik',
+    'title' : 'Blog Post 2',
+    'content' : 'This is the content of 2nd Post.',
+    'date_posted' : 'November 10, 2020'
+  },
+]
 
 def home(request):
-  return HttpResponse('<h1>Blog Home</h1>')
+  context = {
+    'posts' : posts,
+    'title' : 'Home'
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
-  return HttpResponse('<h1>Blog About</h1>')
+  return render(request, 'blog/about.html')
